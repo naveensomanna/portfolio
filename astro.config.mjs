1c1af36d94138beb23eb32103ce47dbbe76f97f6
naveensomanna26 <naveensomanna26@gmail.com>
2024-04-06 11:09:06 +0530

    fixed sharp config issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig ,passthroughImageService} from "astro/config";
  import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [icon()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
