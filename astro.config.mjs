dbba2f69efac82b3f48b52e435dbb829e3fa229a
naveensomanna26 <naveensomanna26@gmail.com>
2024-04-06 12:18:10 +0530

    removed config of icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig ,passthroughImageService} from "astro/config";
- import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [icon({
-    iconDir: 'src/icons',
-  })],
   image: {
     service: passthroughImageService(),
   },
